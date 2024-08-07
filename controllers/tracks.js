f8afed831317a4a467610758233f6a5844046d9e
Zawadi2 <zawadin95@gmail.comm>
2024-08-07 09:26:09 -0500

    added get a single track id rout

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -9,3 +9,19 @@ router.post('/tracks', async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 })
+
+
+
+// Get a single track
+router.get('/tracks/:id', async (req, res) => {
+    try {
+      const track = await Track.findById(req.params.id);
+      if (!track) {
+        res.status(404).json({ message: 'Track not found' });
+      } else {
+        res.status(200).json(track);
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
