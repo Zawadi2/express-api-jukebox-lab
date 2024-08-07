d8b90f20cb147e5cfb8897c69999ca02ff970799
tnleibel <tnleibel@outlook.com>
2024-08-07 11:14:42 -0400

    Added post and get routes to /tracks

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -2,10 +2,25 @@ const express = require('express')
 const router = express.Router()
 const Track = require('../models/track')
 
-router.post('/tracks', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-
+        const track = await Track.create({
+            title: req.body.title,
+            artist: req.body.artist
+        })
+        res.status(201).json({ track })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
+
+router.get('/', async (req, res) => {
+    try {
+        const tracks = await Track.find()
+        res.status(200).json({ tracks })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router;
